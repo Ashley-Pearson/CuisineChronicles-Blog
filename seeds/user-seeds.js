167651d905327ced246f24a52b74e1dfd2ee1b43
Ashley Pearson <ashleypearson@Ashleys-MacBook-Air.local>
2023-02-26 20:28:59 -0700

    add user-seeds

---
+++ seeds/user-seeds.js
@@ -0,0 +1,17 @@
+[
+    {
+      "name": "Reese",
+      "email": "Reese@gmail.com",
+      "password": "password1234!"
+    },
+    {
+      "name": "Charley",
+      "email": "charles@gmail.com",
+      "password": "password1234!"
+    },
+    {
+      "name": "Ashley",
+      "email": "Ash@yahoo.com",
+      "password": "password1234!"
+    }    
+  ]
